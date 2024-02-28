e0b560ac6f3502facececa10ec56b35d85b18683
hulinxiang <648920782@qq.com>
2024-02-28 22:57:02 +1100

    26  finished

---
+++ src/Question26/Solution.java
@@ -0,0 +1,28 @@
+package Question26;
+
+/**
+ * @author ºúÁØÏè
+ */
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 4};
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(nums));
+    }
+}
