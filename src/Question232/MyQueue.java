fce43cb2fbe4f489363087d57809bad6c44b22ab
Linxiang Hu <u7633783@anu.edu.au>
2024-03-04 15:20:53 +1100

    232 finished

---
+++ src/Question232/MyQueue.java
@@ -0,0 +1,57 @@
+package Question232;
+
+import java.util.Stack;
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+public class MyQueue {
+
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while (!s2.empty()) {
+            s1.push(s2.pop());
+        }
+        s1.push(x);
+    }
+
+    public int pop() {
+        int ans;
+        while (!s1.empty()) {
+            s2.push(s1.pop());
+        }
+        ans = s2.pop();
+        while (!s2.empty()) {
+            s1.push(s2.pop());
+        }
+        return ans;
+    }
+
+    public int peek() {
+        int ans;
+        while (!s1.empty()) {
+            s2.push(s1.pop());
+        }
+        ans = s2.peek();
+        while (!s2.empty()) {
+            s1.push(s2.pop());
+        }
+        return ans;
+    }
+
+    public boolean empty() {
+        return s1.empty() && s2.empty();
+    }
+}
