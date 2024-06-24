34232c9a13e771b9a152209966f180b1be795caf
Linxiang Hu <u7633783@anu.edu.au>
2024-06-24 22:34:09 +1000

    503 finished

--- src/Array/Question503/Solution.java
+++ src/Array/Question503/Solution.java
@@ -1,24 +1,30 @@
 package Array.Question503;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * 单调栈
+ */
 public class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = getNextGreaterElement(nums, i, n);
+        Arrays.fill(ans, -1);
+        Deque<Integer> st = new ArrayDeque<>();
+        for (int i = 0; i < 2 * n; i++) {
+            int x = nums[i % n];
+            while (!st.isEmpty() && x > nums[st.peek()]) {
+                // x 是 nums[st.peek()] 的下一个更大元素
+                // 既然 nums[st.peek()] 已经算出答案，则从栈顶弹出
+                ans[st.pop()] = x;
+            }
+            if (i < n) {
+                st.push(i);
+            }
         }
         return ans;
     }
-
-    public int getNextGreaterElement(int[] nums, int i, int n) {
-        int cur = i;
-        for (int k = 0; k < n; k++) {
-            cur = (cur + 1 + n) % n;
-            if (nums[cur] > nums[i]) {
-                return nums[cur];
-            }
-        }
-        return -1;
-    }
-
 }
+
