b4ecfbb992a621492ede17c012f3350a855b6dc0
Linxiang Hu <u7633783@anu.edu.au>
2024-07-15 00:15:16 +1000

    55 finished

--- src/Array/Question55/Solution.java
+++ src/Array/Question55/Solution.java
@@ -1,4 +1,22 @@
 package Array.Question55;
 
 public class Solution {
+
+    public boolean canJump(int[] nums) {
+        int mx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > mx) {
+                break;
+            }
+            mx = Math.max(mx, i + nums[i]);
+
+        }
+        return mx >= nums.length - 1;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().canJump(new int[]{2, 3, 1, 1, 4}));
+    }
 }
