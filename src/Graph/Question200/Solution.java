9c580b38b69843386d5441253e0339bce26cc790
Linxiang Hu <hulinxiang2009@gmail.com>
2024-11-27 19:36:07 +0800

    Question 200 finished

---
+++ src/Graph/Question200/Solution.java
@@ -0,0 +1,42 @@
+package Graph.Question200;
+
+public class Solution {
+
+    int n; // n代表有几行
+    int m; // m代表有几列
+
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        n = grid.length;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    ans++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i == n || j == m || i == -1 || j == -1) return;
+        if (grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println(new Solution().numIslands(grid));
+    }
+}
